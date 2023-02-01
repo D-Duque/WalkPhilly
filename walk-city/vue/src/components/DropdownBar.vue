9e33eee5fbef5434bd5acf4eb2909cd02611f8a7
Emma Nguyen <emma.nguyen.code@gmail.com>
2023-02-01 14:23:57 -0800

    Added filter categories

--- walk-city/vue/src/components/DropdownBar.vue
+++ walk-city/vue/src/components/DropdownBar.vue
@@ -6,8 +6,10 @@
       variant="light"
       class="m-0"
       menu-class="w-100"
+      v-model="currentCategory"
+      @change="filteredCategories"
     >
-      <b-dropdown-item href="#">Restaurants</b-dropdown-item>
+      <b-dropdown-item href="#" value="Restaurant">Restaurants</b-dropdown-item>
       <b-dropdown-item href="#">Museums</b-dropdown-item>
       <b-dropdown-item href="#">Bars</b-dropdown-item>
       <b-dropdown-item href="#">Parks</b-dropdown-item>
@@ -18,7 +20,23 @@
 export default {
   name: "dropdown-bar",
   props: [],
-  methods: {}
+  methods: {
+    filteredCategories() {
+      const filteredLocations = this.$store.getters.nearbyLocations.filter(
+        location =>
+          location.category
+            .toLowerCase()
+            .includes(this.currentCategory.toLowerCase()) ||
+          location.category == ""
+      );
+      this.$store.commit("FILTER_LOCATIONS", filteredLocations);
+    }
+  },
+  data() {
+    return {
+      currentCategory: ""
+    };
+  }
 };
 </script>
 <style scoped>
