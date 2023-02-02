73623f28ac00af09b32c132ebd7fc5b2439698cc
Patricia Kamlley <pkamlley@gmail.com>
2023-02-02 13:41:53 -0800

    working onstyling of two filter versus one filter

--- walk-city/vue/src/components/FilterResults.vue
+++ walk-city/vue/src/components/FilterResults.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="filter-results"
         :class="{ 'filter-on': filteringOn, 'filter-off': !filteringOn, 'one-filter': oneFilter, 'two-filter': twoFilter }">
-        <div id="magnifying-glass"><img src="../assets/search-big.png"></div>
-        <div id="filter-results">
-            <div id="text-result">Philly Cheesesteak</div>
-            <div id="category-result">Restaurants</div>
+        <div id="magnifying-glass"><img v-show="twoFilter" src="../assets/search-big.png"><img v-show="oneFilter"></div>
+        <div id="text-category-results">
+            <div id="text-result">{{ this.$store.state.textFilter }}</div>
+            <div id="category-result">{{ this.$store.state.currentCategory }}</div>
         </div>
     </div>
 </template>
@@ -24,21 +24,32 @@ export default {
 }
 </script>
 <style scoped>
-.filter-on {
-
-    position: fixed;
-    top: 1px;
-    background: #004953;
+#filter-results {
     justify-content: center;
     align-items: center;
     font-size: large;
-    width: 361px;
-    height: auto;
+    color: white;
     background: rgba(0, 73, 83, 0.8);
     border: 1px solid #000000;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 40px;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+
+}
+
+#text-category-results {
+    color: white;
+}
+
+.filter-on {
+    position: fixed;
+    top: 1px;
+    font-size: large;
+    height: auto;
+
 }
 
 .two-filter {
