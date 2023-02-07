738200320274f52ffcf53ea406f12b603d1df705
Emma Nguyen <emma.nguyen.code@gmail.com>
2023-02-07 10:57:03 -0800

    Fixed timestamp

--- walk-city/vue/src/components/HistoryDisplay.vue
+++ walk-city/vue/src/components/HistoryDisplay.vue
@@ -1,73 +1,69 @@
 <template>
-    <div class="history">
-        <div class="history-card">
-            <img id="verified-img" src="../assets/verified-account.png">
-            <h3>{{ locationObject.locationName }}</h3>
-            <h4>{{ entry.checkInTime }}</h4>
-        </div>
+  <div class="history">
+    <div class="history-card">
+      <img id="verified-img" src="../assets/verified-account.png" />
+      <h3>{{ locationObject.locationName }}</h3>
+      <h4>
+        {{ entry.checkInTime.toString().slice(0, 10) }}
+        {{ entry.checkInTime.toString().slice(11, 19) }}
+      </h4>
     </div>
+  </div>
 </template>
 <script>
-
-import locationService from '../services/LocationService';
+import locationService from "../services/LocationService";
 
 export default {
-    components: {
+  components: {},
+  data() {
+    return {
+      locationObject: {}
+    };
+  },
+  props: ["entry"],
 
-    },
-    data() {
-        return {
-            locationObject: {}
-        }
-    },
-    props: ["entry"],
-
-    created() {
-        locationService.getLocationById(this.entry.locationId).then(response => {
-            this.locationObject = response.data
-        })
-
-    }
-}
-
-
+  created() {
+    locationService.getLocationById(this.entry.locationId).then(response => {
+      this.locationObject = response.data;
+    });
+  }
+};
 </script>
 <style scoped>
 div.history {
-    /* display: grid; */
-    border: 1px black solid;
-    border-radius: 6px;
-    padding: 1rem;
-    margin: 10px;
-    background-color: white;
-    color: rgb(0, 73, 83);
-    align-items: center;
-    align-self: center;
+  /* display: grid; */
+  border: 1px black solid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+  background-color: white;
+  color: rgb(0, 73, 83);
+  align-items: center;
+  align-self: center;
 }
 
 div.history p {
-    margin: 20px;
+  margin: 20px;
 }
 
 div.history h3 {
-    display: inline-block;
+  display: inline-block;
 }
 
 div.history h4 {
-    font-size: 1rem;
-    margin: 3px;
-    display: flex;
-    justify-content: center;
+  font-size: 1rem;
+  margin: 3px;
+  display: flex;
+  justify-content: center;
 }
 
 #history-card {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 #verified-img {
-    margin-right: 10px;
-    justify-content: left;
+  margin-right: 10px;
+  justify-content: left;
 }
-
 </style>
--- walk-city/vue/src/views/History.vue
+++ walk-city/vue/src/views/History.vue
@@ -1,52 +1,72 @@
 <template>
-    <div id="history-page">
-        <div class="buttons">
-            <button id="check-in-history-button"
-                :class="{ 'btn-white-outline': showBadges, 'btn-midnight-green': !showBadges }"
-                @click="historyToggle">CHECK-IN HISTORY</button>
-            <button id="badges-button"
-                :class="{ 'btn-white-outline': !showBadges, 'btn-midnight-green': showBadges }"
-                @click="historyToggle">BADGES</button>
-        </div>
-        <history-display v-for="entry in checkInList" v-bind:key="entry.checkInId" v-bind:entry="entry"
-            v-show="!showBadges"></history-display>
-        <badges-display v-show="showBadges"></badges-display>
+  <div id="history-page">
+    <div class="buttons">
+      <button
+        id="check-in-history-button"
+        :class="{
+          'btn-white-outline': showBadges,
+          'btn-midnight-green': !showBadges
+        }"
+        @click="historyToggle"
+      >
+        CHECK-IN HISTORY
+      </button>
+      <button
+        id="badges-button"
+        :class="{
+          'btn-white-outline': !showBadges,
+          'btn-midnight-green': showBadges
+        }"
+        @click="historyToggle"
+      >
+        BADGES
+      </button>
     </div>
+    <history-display
+      v-for="entry in checkInList"
+      v-bind:key="entry.checkInId"
+      v-bind:entry="entry"
+      v-show="!showBadges"
+    ></history-display>
+    <badges-display v-show="showBadges"></badges-display>
+  </div>
 </template>
 <script>
-import HistoryDisplay from '../components/HistoryDisplay.vue';
+import HistoryDisplay from "../components/HistoryDisplay.vue";
 import BadgesDisplay from "../components/BadgesDisplay.vue";
-import checkInService from '../services/CheckInService';
+import checkInService from "../services/CheckInService";
 
 export default {
-    components: {
-        HistoryDisplay,
-        BadgesDisplay
-    },
-    data() {
-        return {
-            showBadges: false,
-            checkInList: []
-        }
-    },
-    methods: {
-        historyToggle() {
-            this.showBadges = !this.showBadges
-        }
-    },
-    created() {
-        checkInService.getCheckInsByUserId(this.$store.state.user.id).then(response => {
-            this.checkInList = response.data;
-        })
+  components: {
+    HistoryDisplay,
+    BadgesDisplay
+  },
+  data() {
+    return {
+      showBadges: false,
+      checkInList: []
+    };
+  },
+  methods: {
+    historyToggle() {
+      this.showBadges = !this.showBadges;
     }
-}
+  },
+  created() {
+    checkInService
+      .getCheckInsByUserId(this.$store.state.user.id)
+      .then(response => {
+        this.checkInList = response.data;
+      });
+  }
+};
 </script>
 <style scoped>
-    .buttons {
-        margin-top: 1rem;
-        margin-left: 1rem;
-        display: flex;
-        justify-content: center;
-        gap: 1rem;
-    }
+.buttons {
+  margin-top: 1rem;
+  margin-left: 1rem;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
 </style>
