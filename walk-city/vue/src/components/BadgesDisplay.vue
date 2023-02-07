bdd2677bbda1690ac74556db5a8dbdef5dccf806
Hong Le <101665760+honglebs@users.noreply.github.com>
2023-02-07 09:12:39 -0800

    Added some CSS to BadgesDisplay

--- walk-city/vue/src/components/BadgesDisplay.vue
+++ walk-city/vue/src/components/BadgesDisplay.vue
@@ -1,6 +1,8 @@
 <template >
-    <div>
-        I am badges
+    <div class="badge">
+        <div class="badge-card"> Hi</div>
+        <div class="badge-image"></div>
+        <!-- <img id="badge-img" src="../assets/badge.png"> -->
     </div>
 </template>
 <script>
@@ -8,6 +10,42 @@ export default {
 
 }
 </script>
-<style >
+<style scoped>
+div.badge {
+    display: grid;
+    /* border: 1px black solid; */
+    border-radius: 6px;
+    padding: 1rem;
+    margin: 10px;
+    /* background-color: white; */
+    color: rgb(0, 73, 83);
+    align-items: center;
+    align-self: center;
+}
+
+div.badge p {
+    margin: 20px;
+}
+
+div.badge h3 {
+    display: inline-block;
+}
+
+div.badge h4 {
+    font-size: 1rem;
+}
+
+.badge-card {
+    background: #4FD695;
+    border: 1px solid #000000;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    width: 146px;
+    height: 158px;
+    border-radius: 6px;
+}
+
+/* #image-badge {
+    content:url()
+} */
 
 </style>
