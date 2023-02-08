2f6d050c626a3eb245615b8f17c3735110346b11
Daniel Duque <dduque37@gmail.com>
2023-02-08 12:47:49 -0800

    More work on badge modal

--- walk-city/vue/src/components/BadgesDisplay.vue
+++ walk-city/vue/src/components/BadgesDisplay.vue
@@ -1,82 +1,109 @@
 <template >
-    <div class="badge-card" @click="badgeClicked(badge)"> {{badge.badgeName}}
-        <img :src="require(`../assets/${badge.badgeImage}.png`)" >
-      <b-modal :id="`badge-modal-`+ badge.badgeId" centered>
-            <h1>{{badge.badgeName}}</h1>
-            <p>Test</p>
-        </b-modal>
-    </div>    
+  <div
+    class="badge-card"
+    :class="{ 'not-earned-card': !isEarned }"
+    @click="badgeClicked(badge)"
+  >
+    {{ badge.badgeName }}
+    <img :src="require(`../assets/${badge.badgeImage}.png`)" />
+    <b-modal
+      :id="`badge-modal-` + badge.badgeId"
+      
+      centered
+      :title="badge.badgeName"
+      hide-footer
+      hide-header
+      text-center
+    >
+      <div id="modal-details">
+        <img :src="require(`../assets/${badge.badgeImage}.png`)" alt="" />
+        <p>{{ badge.description }}</p>
+      </div>
+    </b-modal>
+  </div>
 </template>
 <script>
 // import badgesServices from "../services/BadgesServices";
 
 export default {
-    // components: {},
-    data () {
-        return {
-            // badgeObject: {},
-            name: '',
-            image: null,
-            description: '',
-        };
+  // components: {},
+  data() {
+    return {
+      // badgeObject: {},
+      name: "",
+      image: null,
+      description: "",
+    };
+  },
+  props: ["badge", "userBadgeList"],
+  methods: {
+    badgeClicked: function (badge) {
+      this.name = badge.badgeName;
+      this.image = badge.badgeImage;
+      this.$bvModal.show("badge-modal-" + badge.badgeId);
     },
-    props: ["badge", "userBadgeList"],
-    methods: {
-        badgeClicked: function(badge) {
-            this.name = badge.badgeName;
-            this.image = badge.badgeImage;
-            this.$bvModal.show('badge-modal-' + badge.badgeId)
-        }
+  },
+  mounted() {},
+  computed: {
+    isEarned() {
+      return (
+        this.userBadgeList.filter(
+          (entry) => entry.badgeId == this.badge.badgeId
+        ).length == 1
+      );
     },
-    mounted() {
-
-    },
-    computed: {
-        isEarned() {
-            return this.userBadgeList.filter(entry => entry.badgeId == this.badge.badgeId).length == 1
-        }
-    }
-}
-
-
+  },
+};
 </script>
 <style scoped>
+
 div.badge {
-    display: grid;
-    border-radius: 6px;
-    padding: 1rem;
-    margin: 10px;
-    color: rgb(0, 73, 83);
-    grid-template-columns: repeat(2, 1fr);
-    column-gap: 1rem;
-    row-gap: 1rem;
+  display: grid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+  color: rgb(0, 73, 83);
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 1rem;
+  row-gap: 1rem;
 }
 
 div.badge p {
-    margin: 20px;
+  margin: 20px;
 }
 
 div.badge h3 {
-    display: inline-block;
+  display: inline-block;
 }
 
 div.badge h4 {
-    font-size: 1rem;
+  font-size: 1rem;
 }
 
 .badge-card {
-    background: #4fd695;
-    border: 1px solid #000000;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    width: 146px;
-    height: 158px;
-    border-radius: 6px;
-    display: flex;
-    justify-content: center;
-    padding: 1rem;
+  background: #4fd695;
+  border: 1px solid #000000;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 146px;
+  height: 158px;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
 }
 
 .not-earned-card {
-    -webkit-filter: grayscale(100%);
+  -webkit-filter: grayscale(100%);
+}
+#modal-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+</style>
+<style >
+.modal-backdrop {
+    opacity: 0.25 !important;
 }
 </style>
