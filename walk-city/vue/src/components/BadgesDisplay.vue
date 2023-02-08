7705436fccb1bc0c01d3c656ff6532eaffd68d9b
Emma Nguyen <emma.nguyen.code@gmail.com>
2023-02-08 09:50:10 -0800

    Fixed db

--- walk-city/vue/src/components/BadgesDisplay.vue
+++ walk-city/vue/src/components/BadgesDisplay.vue
@@ -1,70 +1,66 @@
-<template >
-    <div class="badge-card"> {{badge.badgeName}}
-        <img :src="require(`../assets/${badge.badgeImage}.png`)" >
-    </div>    
+<template>
+  <div class="badge-card">
+    {{ badge.badgeName }}
+    <img :src="require(`../assets/${badge.badgeImage}.png`)" />
+  </div>
 </template>
 <script>
 // import badgesServices from "../services/BadgesServices";
 
 export default {
-    // components: {},
-    // data () {
-    //     return {
-    //         badgeObject: {}
-    //     };
-    // },
-    props: ["badge"],
-    // methods: {
-    //     created() {
-    //     badgesServices.getBadgesByUserId(this.entry.userId).then(response => {
-    //     this.badgeObject = response.data;
-    // });
-    // }
+  // components: {},
+  // data () {
+  //     return {
+  //         badgeObject: {}
+  //     };
+  // },
+  props: ["badge"]
+  // methods: {
+  //     created() {
+  //     badgesServices.getBadgesByUserId(this.entry.userId).then(response => {
+  //     this.badgeObject = response.data;
+  // });
+  // }
 
-    // }
-}
-
+  // }
+};
 </script>
 <style scoped>
 div.badge {
-    display: grid;
-    border-radius: 6px;
-    padding: 1rem;
-    margin: 10px;
-    color: rgb(0, 73, 83);
-    grid-template-columns: repeat(2, 1fr);
-    column-gap: 1rem;
-    row-gap: 1rem;
+  display: grid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+  color: rgb(0, 73, 83);
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 1rem;
+  row-gap: 1rem;
 }
 
 div.badge p {
-    margin: 20px;
+  margin: 20px;
 }
 
 div.badge h3 {
-    display: inline-block;
+  display: inline-block;
 }
 
 div.badge h4 {
-    font-size: 1rem;
+  font-size: 1rem;
 }
 
 .badge-card {
-    background: #4FD695;
-    border: 1px solid #000000;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    width: 146px;
-    height: 158px;
-    border-radius: 6px;
-    display: flex;
-    justify-content: center;
-    padding: 1rem;
-
+  background: #4fd695;
+  border: 1px solid #000000;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 146px;
+  height: 158px;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
 }
 
 .not-earned-card {
-    
 }
-
-
 </style>
--- walk-city/vue/src/views/History.vue
+++ walk-city/vue/src/views/History.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="history-page">
     <div class="buttons">
-    <div>
-      <router-link id="back-button" to="/">
-        <img src="../assets/back-arrow.png" />
-      </router-link>
-    </div>
+      <div>
+        <router-link id="back-button" to="/">
+          <img src="../assets/back-arrow.png" />
+        </router-link>
+      </div>
       <button
         id="check-in-history-button"
         :class="{
@@ -26,22 +26,21 @@
       >
         BADGES
       </button>
-
     </div>
     <history-display
-        v-for="entry in checkInList"
-        v-bind:key="entry.checkInId"
-        v-bind:entry="entry"
-        v-show="!showBadges"
+      v-for="entry in checkInList"
+      v-bind:key="entry.checkInId"
+      v-bind:entry="entry"
+      v-show="!showBadges"
     ></history-display>
     <div class="badge-container">
-    <badges-display 
+      <badges-display
         v-show="showBadges"
-        v-for="badge in badgeList" 
+        v-for="badge in badgeList"
         v-bind:key="badge.badgeId"
-        v-bind:badge="badge">
-
-    </badges-display>
+        v-bind:badge="badge"
+      >
+      </badges-display>
     </div>
   </div>
 </template>
@@ -60,7 +59,7 @@ export default {
     return {
       showBadges: false,
       checkInList: [],
-        badgeList: []
+      badgeList: []
     };
   },
   methods: {
@@ -75,10 +74,8 @@ export default {
         this.checkInList = response.data;
       });
 
-    badgesService
-        .getAllBadges()
-        .then(response => {
-        this.badgeList = response.data;
+    badgesService.getAllBadges().then(response => {
+      this.badgeList = response.data;
     });
   }
 };
@@ -93,17 +90,17 @@ export default {
 }
 
 .badge-container {
-    display: grid;
-    border-radius: 6px;
-    padding: 1rem;
-    margin: 10px;
-    color: rgb(0, 73, 83);
-    grid-template-columns: repeat(2, 1fr);
-    column-gap: 1rem;
-    row-gap: 1rem;
+  display: grid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+  color: rgb(0, 73, 83);
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 1rem;
+  row-gap: 1rem;
 }
 
 .greyscale {
-    -webkit-filter: grayscale(100%);
+  -webkit-filter: grayscale(100%);
 }
 </style>
