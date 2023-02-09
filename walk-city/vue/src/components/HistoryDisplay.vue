bbd9301a1ae10443d1fa6df231923746fa4c7a02
Hong Le <101665760+honglebs@users.noreply.github.com>
2023-02-08 20:34:47 -0800

    Struggled with CSS for a bit but got some things to display -- still need to align the rows corrently

--- walk-city/vue/src/components/HistoryDisplay.vue
+++ walk-city/vue/src/components/HistoryDisplay.vue
@@ -1,17 +1,18 @@
 <template>
-  <div> <img class="park-overlay" src="../assets/park-icon.png" />
-    <div class="history">
-      <div class="history-card">
-        <img id="verified-img" src="../assets/verified-account.png" />
-        <h3>{{ locationObject.locationName }}</h3>
-        <h4>
-          {{ entry.checkInTime.toString().slice(0, 10) }}
-          {{ entry.checkInTime.toString().slice(11, 19) }}
-        </h4>
-      </div>
-    </div>
-  </div>
+  <div>
+      <div class="history">
+        <div class="history-card">
+          <div id="park-overlay"><img class="absolute" src="../assets/museum-icon.png"></div>
+          <h3>{{ locationObject.locationName }}</h3>
+          <h4>
+            {{ entry.checkInTime.toString().slice(0, 10) }}
+            {{ entry.checkInTime.toString().slice(11, 19) }}
+          </h4>
+          <img id="verified-img" src="../assets/verified-account.png" />
+        </div>
 
+      </div>
+  </div>
 
 </template>
 <script>
@@ -35,7 +36,8 @@ export default {
 </script>
 <style scoped>
 div.history {
-  border: 1px black solid;
+  /* border: 1px black solid; */
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 6px;
   padding: 1rem;
   margin: 10px;
@@ -43,7 +45,16 @@ div.history {
   color: rgb(0, 73, 83);
   align-items: center;
   align-self: center;
+  overflow: hidden;
+  
+  /* background-image: url(../assets/park-icon.png); */
+  /* background-repeat: no-repeat; */
+  /* background-position: */
+  /* transform: rotate(32.7deg); */
+  
+  /* background: no-repeat; */
 }
+
 
 div.history p {
   margin: 20px;
@@ -51,31 +62,52 @@ div.history p {
 
 div.history h3 {
   display: inline-block;
-  font-size: 1.5rem;
+  font-size: 1rem;
 }
 
 div.history h4 {
-  font-size: 1rem;
+  font-size: .7rem;
   margin: 3px;
-  margin-left: 3.5rem;
-  display: flex;
-  justify-content: left;
+  margin-left: 1.5rem;
+  /* display: flex;
+  justify-content: center; */
 }
 
-#history-card {
+.history-card {
   display: flex;
+  /* display: grid;
+  grid-template-columns: 2fr 1fr 1fr */
   justify-content: center;
+  flex-direction: row;
   place-items: center;
 
 }
 
 #verified-img {
-  margin-right: 10px;
+  /* margin-right: 10px; */
+  /* justify-self: right; */
+  width: 30px;
   
 }
 
-.park-overlay {
+img.absolute {
+  /* left: 100%; */
+  /* margin-left: -200px; */
+  /* position: absolute; */
+}
+
+#park-overlay {
+  background-image:url(../assets/museum-icon.png);
+  background-repeat: no-repeat;
+  position: relative;
   transform: rotate(32.7deg);
+  height: 100px;
+  width: 100px;
+  margin-left: -45px;
+  margin-bottom: -60px;
+  margin-top: -80px;
+
+  /* overflow: hidden; */
 }
 
 
